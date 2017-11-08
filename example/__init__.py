04229e3f9cb6e17cdfc819f2343fe25b7f26dd07
Ted Brandston <tbrandston@neverware.com>
2017-11-08 15:31:17 -0500

    Adding AWS::DynamoDB::Table and an AWS::Serverless::Function accessing it
    When running locally the environment variable is, understandably, blank.

--- example/__init__.py
+++ example/__init__.py
@@ -21,3 +21,15 @@ def echo(event, context):
             'environ': { key: os.environ[key] for key in os.environ }
         }, indent=2)
     }
+
+
+def use_table(event, context):
+    # It makes sense that this is empty when running locally,
+    # but if my function needs to access a table I need to handle
+    # this case _somehow_.
+    table = os.environ['TABLE']
+
+    return {
+        'statusCode': 200 if table else 500,
+        'body': json.dumps({'table': table})
+    }
